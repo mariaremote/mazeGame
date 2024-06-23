5833569e54700c2b86f3c708a338ab3a06a4fe91
Maria <123465523+mariaremote@users.noreply.github.com>
2024-06-23 15:32:53 +0200

    finish game functionality

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
-// todo: extract as game configuration
+// todo: extract as game configurations
 const hat = "^";
 const hole = "O";
 const fieldCharacter = "░";
@@ -8,27 +8,22 @@ const pathCharacter = "*";
 const fieldHeight = 10;
 const fieldWidth = 13;
 const holeCoverage = 0.2;
+const textDisplayTime = 3000;
 
-const dummyField = [
-  ["*", "░", "O"],
-  ["░", "O", "░"],
-  ["░", "^", "░"],
-];
 class Field {
-  constructor(height, width, difficulty) {
-    this.field = this.generateField(height, width, difficulty);
-    this.currentLocation = this.getStartLocation();
+  constructor(field) {
+    this.field = field;
     this.gameOver = false;
+    this.currentLocation = this.getStartLocation();
+    this.nextLocation = { ...this.currentLocation };
   }
-
   play() {
     while (!this.gameOver) {
       this.print();
-      this.updateLocation();
+      this.moveCharacter();
     }
   }
-  // todo: refactor how field is built. Get hat and start position first. Then get the amount of holes by percentage from height/width, get random coordinates for each hole, and place them on the field. Check hole locations each to avoid overlap with hat or start location.
-  generateField(height, width, percentage) {
+  static generateField(height, width, percentage) {
     const field = new Array(height).fill(0).map(() => new Array(width).fill(0));
     for (let i = 0; i < height; i++) {
       for (let j = 0; j < width; j++) {
@@ -43,9 +38,6 @@ class Field {
       x: Math.floor(Math.random() * width),
       y: Math.floor(Math.random() * height),
     }; // avoid overlap hat and start location
-    console.log(
-      `Hat: (${hatLocation.x},${hatLocation.y})\nStart: (${startLocation.x},${startLocation.y})`
-    );
     if (
       hatLocation.x === startLocation.x &&
       hatLocation.y === startLocation.y
@@ -62,7 +54,18 @@ class Field {
       console.log(row.join(""));
     });
   }
-
+  moveCharacter() {
+    this.getNextLocation();
+    if (this.checkLocationOutOfBounds()) {
+      this.nextLocation = { ...this.currentLocation }; // resets next location
+      this.moveCharacter();
+    } else if (this.checkLocationHatOrHole()) {
+      this.gameOver = true;
+      process.exit();
+    }
+    this.currentLocation = { ...this.nextLocation };
+    this.field[this.currentLocation.y][this.currentLocation.x] = pathCharacter;
+  }
   getStartLocation() {
     for (let y = 0; y < this.field.length; y++) {
       for (let x = 0; x < this.field[y].length; x++) {
@@ -72,69 +75,63 @@ class Field {
       }
     }
   }
-  getMove() {
+  getNextLocation() {
+    // todo: refactor to use arrow keys (no need to press enter)
     const move = prompt("Which way?").toUpperCase();
     switch (move) {
       case "D":
-        return { x: 0, y: 1 };
+        this.nextLocation.y += 1;
+        break;
+      // return { x: 0, y: 1 };
       case "U":
-        return { x: 0, y: -1 };
+        this.nextLocation.y -= 1;
+        break;
+      // return { x: 0, y: -1 };
       case "L":
-        return { x: -1, y: 0 };
+        this.nextLocation.x -= 1;
+        break;
+      // return { x: -1, y: 0 };
       case "R":
-        return { x: 1, y: 0 };
+        this.nextLocation.x += 1;
+        break;
+      // return { x: 1, y: 0 };
       default:
         console.log("Enter D(down), L(left), U(up), or R(right)");
-        return this.getMove();
+        return this.getNextLocation();
     }
   }
-  getNextLocation() {
-    const nextLocation = this.getMove();
-    return {
-      x: this.currentLocation.x + nextLocation.x,
-      y: this.currentLocation.y + nextLocation.y,
-    };
-  }
-  checkLocationOutOfBounds(nextLocation) {
-    // this does not work yet
-    const { x, y } = nextLocation;
-    if (x < 0 || y < 0 || y > this.field.length || x > this.field[y].length) {
+  checkLocationOutOfBounds() {
+    if (
+      this.nextLocation.x < 0 ||
+      this.nextLocation.y < 0 ||
+      this.nextLocation.y >= this.field.length ||
+      this.nextLocation.x >= this.field[0].length
+    ) {
+      console.log("Out of bounds!");
       return true;
     }
-    return false;
   }
-  checkLocationHatOrHole(nextLocation) {
-    const { x, y } = nextLocation;
-    if (this.field[y][x] === hat) {
+  checkLocationHatOrHole() {
+    const currentLocation =
+      this.field[this.nextLocation.y][this.nextLocation.x];
+    if (currentLocation === hat) {
       console.log("Congrats! You found your hat!");
       return true;
-    } else if (this.field[y][x] === hole) {
+    } else if (currentLocation === hole) {
       console.log("You fell into a hole! Game Over!");
       return true;
     }
   }
-  updateLocation() {
-    this.currentLocation = this.getNextLocation();
-    if (this.checkLocationOutOfBounds(this.currentLocation)) {
-      console.log("Out of bounds!");
-      return false;
-    }
-    if (this.checkLocationHatOrHole(this.currentLocation)) {
-      this.gameOver = true;
-      setTimeout(() => {
-        console.clear();
-      }, 3000);
-      return true;
-    }
-    this.field[this.currentLocation.y][this.currentLocation.x] = pathCharacter;
-  }
 }
 
 // Main Game Loop
-const game = new Field(fieldHeight, fieldWidth, holeCoverage);
+const game = new Field(
+  Field.generateField(fieldHeight, fieldWidth, holeCoverage)
+);
 game.play();
 
 // Tests
+// todo: write proper test files
 
 // console.log(generateField(4, 4, 0.2));
 
